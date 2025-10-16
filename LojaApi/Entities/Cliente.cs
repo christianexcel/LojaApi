bb1cb388dc11837a6a13863be9ec260b667e6295
Christian Rafael <ti03@itb.ind.br>
2025-10-16 09:14:32 -0300

    Ajuste DTOs de clientes

--- LojaApi/Entities/Cliente.cs
+++ LojaApi/Entities/Cliente.cs
@@ -6,22 +6,10 @@ namespace LojaApi.Entities
     [Table("TB_CLIENTES")]
     public class Cliente
     {
-        [Key]
-        [Column("id_cliente")]
         public int Id { get; set; }
-        [Column("nome_cliente")]
-        [Required(ErrorMessage = "O nome do cliente é obrigatório.")]
-        [MaxLength(100, ErrorMessage = "O nome do cliente não pode exceder 100 caracteres.")]
-        [MinLength(3, ErrorMessage = "O nome do cliente deve ter no mínimo 3 caracteres.")]
         public string Nome { get; set; } = string.Empty;
-        [Column("email_cliente")]
-        [Required(ErrorMessage = "O email do cliente é obrigatório.")]
-        [EmailAddress(ErrorMessage = "O email do cliente deve ser válido.")]
         public string Email { get; set; } = string.Empty;
-        [Column("ativo")]
-        [Required]
         public bool Ativo { get; set; }
-        [Column("data_cadastro")]
         public DateTime DataCadastro { get; set; }
         public Endereco? Endereco { get; set; }
     }
--- LojaApi/Infra/DTOs/CriarClienteDto.cs
+++ LojaApi/Infra/DTOs/CriarClienteDto.cs
@@ -5,10 +5,14 @@ namespace LojaApi.Infra.DTOs;
 
 public class CriarClienteDto
 {
-    [Required]
+
+    [Required(ErrorMessage = "O nome do cliente é obrigatório.")]
+    [MaxLength(100, ErrorMessage = "O nome do cliente não pode exceder 100 caracteres.")]
+    [MinLength(3, ErrorMessage = "O nome do cliente deve ter no mínimo 3 caracteres.")]
     public string Nome { get; set; } = string.Empty;
-    [Required]
-    [EmailAddress]
+    [Required(ErrorMessage = "O email do cliente é obrigatório.")]
+    [EmailAddress(ErrorMessage = "O email do cliente deve ser válido.")]
     public string Email { get; set; } = string.Empty;
+    [Required(ErrorMessage = "O endereço do cliente é obrigatório.")]
     public EnderecoDto? Endereco { get; set; }
 }
